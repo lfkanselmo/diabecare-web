591ca261b34afd82c8e9d5697b68b89d6f5e597c
Anselmo Ortega <lfkanselmo@gmail.com>
2026-06-11 18:23:30 -0500

    feat: configuracion de PWA para manejo de cache

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID, APP_INITIALIZER } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -11,33 +11,9 @@ import localeEs from '@angular/common/locales/es';
 import { routes } from './app.routes';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
-import { MetadataService } from './core/services/metadata.service';
-import { AuthService } from './core/auth/auth.service';
+import { provideServiceWorker } from '@angular/service-worker';
 
 registerLocaleData(localeEs);
-
-function initializeMetadata(
-  metadataService: MetadataService,
-  authService: AuthService
-): () => Promise<void> {
-  return () => new Promise((resolve) => {
-    if (!authService.isAuthenticated()) {
-      resolve();
-      return;
-    }
-    metadataService.loadAll();
-    const interval = setInterval(() => {
-      if (metadataService.loaded()) {
-        clearInterval(interval);
-        resolve();
-      }
-    }, 50);
-    setTimeout(() => {
-      clearInterval(interval);
-      resolve();
-    }, 5000);
-  });
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -50,11 +26,9 @@ export const appConfig: ApplicationConfig = {
     provideStoreDevtools({ maxAge: 25, logOnly: false }),
     { provide: LOCALE_ID, useValue: 'es' },
     provideEchartsCore({ echarts: () => import('echarts') }),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeMetadata,
-      deps: [MetadataService, AuthService],
-      multi: true
-    }
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };

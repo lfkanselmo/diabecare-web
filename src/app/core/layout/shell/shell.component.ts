83aa7b5fc9eac509a10cb4f9de2e5f7d32beed81
Anselmo Ortega <lfkanselmo@gmail.com>
2026-06-10 20:36:41 -0500

    bug: correccion bug cargas de metadata

--- src/app/core/layout/shell/shell.component.ts
+++ src/app/core/layout/shell/shell.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { MetadataService } from '../../services/metadata.service';
 
 @Component({
     selector: 'app-shell',
@@ -10,8 +11,15 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
     templateUrl: './shell.component.html',
     styleUrl: './shell.component.scss'
 })
-export class ShellComponent {
+export class ShellComponent implements OnInit {
+
+    private readonly metadataService = inject(MetadataService);
+
     sidebarOpen = true;
+
+    ngOnInit(): void {
+        this.metadataService.loadAll();
+    }
 
     toggleSidebar(): void {
         this.sidebarOpen = !this.sidebarOpen;
